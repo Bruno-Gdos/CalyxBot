26ccfb913e72961be70f008d51dd98f0f40b8f3a
Bruno Gabriel dos Santos Costa <87917525+Bruno-Gdos@users.noreply.github.com>
2023-09-02 22:45:40 -0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import os
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
+
+def run_discord_bot():
+    client = commands.Bot(command_prefix="$", intents = discord.Intents.default())
+
+    @client.event
+    async def on_ready():
+        print("Calyx is ready.")
+        try:
+            synced = await client.tree.sync()
+            print ("Synced commands: " + str(synced))
+        except Exception as e:
+            print("Error syncing commands: " + str(e))
+
+    @client.tree.command(name = "youtube" , description = "Downloads the youtube video")
+    @app_commands.describe(thing_to_search = "Video Link" )
+    async def youtube(interaction: discord.Interaction, thing_to_search: str):
+        await interaction.response.send_message(f'')
+
+    @client.tree.command(name = "hello" , description = "Says hello to the user")
+    async def hello(interaction: discord.Interaction):
+        await interaction.response.send_message(f"Hello {interaction.user.mention}!")
+
+    client.run(BOT_TOKEN)
---
+++ youtube.py
@@ -0,0 +1,4 @@
+import pytube
+
+def download_video():
+    print("Enter the link of the video you want to download")
